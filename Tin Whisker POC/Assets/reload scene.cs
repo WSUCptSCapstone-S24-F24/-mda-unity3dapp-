ae2caa3fca2349d1b07406208626a25946aeff67
Gageu <gage.unruh@wsu.edu>
2023-03-07 18:51:15 -0800

    scene reload
    working

--- Tin Whisker POC/Assets/reload scene.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine.SceneManagement;
-
-    void ReloadScene()
-{
-    int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-    SceneManager.LoadScene(currentSceneIndex);
-}
