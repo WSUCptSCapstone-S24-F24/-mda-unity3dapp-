4f824172aadb5ade2837598f9b071f1d9ab52f28
Gageu <gage.unruh@wsu.edu>
2023-03-28 13:14:23 -0700

    Scene restart and UI working

--- Tin Whisker POC/Assets/SceneHandler.cs
+++ Tin Whisker POC/Assets/SceneHandler.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using TMPro;
 using System.Collections;
 using System.Collections.Generic;
+using SimInfo;
 
 public class SceneHandler : MonoBehaviour
 {
@@ -16,23 +17,51 @@ public class SceneHandler : MonoBehaviour
     public TMP_InputField WhiskerCounteText;
     public TMP_InputField LengthSigmaText;
     public TMP_InputField LengthMuText;
-    public float whiskerCount = 1000.0f;
-    public float lengthSigma = 0.5f;
-    public float lengthMu = 0.005f;
+    public TMP_InputField WidthSigmaText;
+    public TMP_InputField WidthMuText;
+    public TMP_InputField SpawnAreaSizeText;
+    public SimState simState;
+
+    string jsonPath;
 
     public void Start()
     {
+        jsonPath = Application.persistentDataPath + "/SimState.json";
+
+        if (System.IO.File.Exists(jsonPath))
+        {
+            // JSON folder exists, read data from file and initialize SimState object
+            string jsonString = System.IO.File.ReadAllText(jsonPath);
+            simState = JsonUtility.FromJson<SimState>(jsonString);
+        }
+        else
+        {
+            // JSON folder doesn't exist, create SimState object with default constructor
+            simState = new SimState();
+            simState.SaveSimToJSON(jsonPath);
+        }
+
+        WhiskerCounteText.text =simState.WhiskerCount.ToString();
+        LengthSigmaText.text = simState.LengthSigma.ToString();
+        LengthMuText.text = simState.LengthMu.ToString();
+        WidthSigmaText.text = simState.WidthSigma.ToString();
+        WidthMuText.text = simState.WidthMu.ToString();
+        SpawnAreaSizeText.text = simState.spawnAreaSize.ToString();
         
         LoadScene(sceneNum);
         // Set the default value of the text field
-        WhiskerCounteText.text = "1000.0";
-        LengthSigmaText.text = "0.5";
-        LengthMuText.text = "0.005";
-        
+
+
         // Get the float value from the text field
-        if (float.TryParse(WhiskerCounteText.text, out float result))
+        getSimInputs(); 
+
+    }
+
+
+    public void getSimInputs(){
+        if (int.TryParse(WhiskerCounteText.text, out int result))
         {
-            whiskerCount = result;
+            simState.WhiskerCount = result;
         } else
         {
             Debug.Log("Whisker Count is not a float");
@@ -40,7 +69,7 @@ public class SceneHandler : MonoBehaviour
 
         if (float.TryParse(LengthSigmaText.text, out float result2))
         {
-            lengthSigma = result2;
+            simState.LengthSigma = result2;
         }
         else
         {
@@ -49,21 +78,52 @@ public class SceneHandler : MonoBehaviour
 
         if (float.TryParse(LengthMuText.text, out float result3))
         {
-            lengthMu = result3;
+            simState.LengthMu = result3;
         }
         else
         {
             Debug.Log("Length Mu is not a float");
         }
 
-    }
+        if (float.TryParse(WidthSigmaText.text, out float result4))
+        {
+            simState.WidthSigma = result4;
+        }
+        else
+        {
+            Debug.Log("Width Sigma is not a float");
+        }
 
+        if (float.TryParse(WidthMuText.text, out float result5))
+        {
+            simState.WidthMu = result5;
+        }
+        else
+        {
+            Debug.Log("Width Mu is not a float");
+        }
+
+        if (float.TryParse(SpawnAreaSizeText.text, out float result6))
+        {
+            simState.spawnAreaSize = result6;
+        }
+        else
+        {
+            Debug.Log("Spawn Area Size is not a float");
+        }
+    }
 
     public void LoadScene(int buildnum)
     {
+        getSimInputs();
+        StartCoroutine(simState.SaveSimToJSON(jsonPath));
         if (!isSceneLoaded)
         {
             StartCoroutine(LoadSceneAsync(buildnum));
+        }
+        else
+        {
+            ReloadScene(buildnum);
         }
     }
 
@@ -80,8 +140,9 @@ public class SceneHandler : MonoBehaviour
         isSceneLoaded = true;
     }
 
-    public void ReloadScene()
+    public void ReloadScene(int buildnum)
     {
+        loadedScene = SceneManager.GetSceneByBuildIndex(buildnum);
         if(isSceneLoaded)
         {
             SceneManager.UnloadSceneAsync(loadedScene);
@@ -93,12 +154,12 @@ public class SceneHandler : MonoBehaviour
     IEnumerator ReloadSceneAsync()
     {
         AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(loadedScene.name, LoadSceneMode.Additive);
-
         while (!asyncLoad.isDone)
         {
             yield return null;
         }
+        loadedScene = SceneManager.GetSceneByBuildIndex(1);
 
-        SceneManager.SetActiveScene(SceneManager.GetSceneByName(loadedScene.name));
+        SceneManager.SetActiveScene(loadedScene);
     }
 }
--- Tin Whisker POC/Assets/WireSim.cs
+++ Tin Whisker POC/Assets/WireSim.cs
@@ -2,39 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using SimInfo;
 
 public class WireSim : MonoBehaviour
 {
+    public SimState simState;
     public GameObject cylinder;
-    public int cylinderCount = 10;
-    public float spawnAreaSize = 5f;
-    public float heightAboveCircuitBoard = 10f;
-    public float LengthmuIn = 0.5f;
-    public float LengthsigmaIn = 0.5f;
-    public float WidthmuIn = 0.5f;
-    public float WidthsigmaIn = 0.5f;
-
 
     private void Start()
     {
+        string jsonPath = Application.persistentDataPath + "/SimState.JSON";  // replace with your desired JSON folder path
+
+
+        if (System.IO.File.Exists(jsonPath))
+        {
+            // JSON folder exists, read data from file and initialize SimState object
+            string jsonString = System.IO.File.ReadAllText(jsonPath);
+            Debug.Log("JSON file exists");
+            Debug.Log("JSON path:\n" + jsonPath);
+            Debug.Log("JSON string:\n" + jsonString);
+            simState = JsonUtility.FromJson<SimState>(jsonString);
+        }
+        else
+        {
+            // JSON folder doesn't exist, create SimState object with default constructor
+            simState = new SimState();
+            Debug.Log("JSON not found\nSaving class to JSON");
+            simState.SaveSimToJSON(jsonPath);
+        }
 
         //print current scene name
         Debug.Log("Current scene is:" + UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
 
         // Open the CSV file for writing
-        StreamWriter writer = new StreamWriter(Application.dataPath + "/../Data/cylinder_lengths.csv");
+        StreamWriter writer = new StreamWriter(Application.persistentDataPath + "/cylinder_lengths.csv");
 
         // Write the header row
         writer.WriteLine("Cylinder Index,Length,Width");
         Vector3 originalScale = cylinder.transform.localScale;
-        for (int i = 0; i < cylinderCount; i++)
+        for (int i = 0; i < simState.WhiskerCount; i++)
         {
-            Vector3 spawnPosition = new Vector3(Random.Range(-spawnAreaSize, spawnAreaSize), heightAboveCircuitBoard, Random.Range(-spawnAreaSize, spawnAreaSize));
+            Vector3 spawnPosition = new Vector3(Random.Range(-simState.spawnAreaSize, simState.spawnAreaSize), simState.heightAboveCircuitBoard, Random.Range(-simState.spawnAreaSize, simState.spawnAreaSize));
             Quaternion spawnRotation = Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360));
             GameObject newCylinder = Instantiate(cylinder, spawnPosition, spawnRotation);
 
-            float lengthMultiplier = LognormalRandom(LengthmuIn, LengthsigmaIn);
-            float widthMultiplier = LognormalRandom(WidthmuIn, WidthsigmaIn);
+            float lengthMultiplier = LognormalRandom(simState.LengthMu, simState.LengthSigma);
+            float widthMultiplier = LognormalRandom(simState.WidthMu, simState.WidthSigma);
             newCylinder.transform.localScale = new Vector3(originalScale.x * widthMultiplier, originalScale.y * lengthMultiplier, originalScale.z * widthMultiplier);
 
             // Write the cylinder index and length to the CSV file
