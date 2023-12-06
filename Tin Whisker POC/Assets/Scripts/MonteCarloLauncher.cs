1b6f52340c243fe97b402caa1d3d25e77ff0e870
Gageu <gage.unruh@wsu.edu>
2023-12-06 15:09:43 -0800

    relative path for MC

--- Tin Whisker POC/Assets/Scripts/MonteCarloLauncher.cs
+++ Tin Whisker POC/Assets/Scripts/MonteCarloLauncher.cs
@@ -6,23 +6,45 @@ using System.IO;
 public class MonteCarloLauncher : MonoBehaviour
 {
     public int numSimulations = 2;
-    public string unityAppPath = "C:/Users/Gage Unruh/Tin Whisker POC/Builds/Final/Tin Whisker POC.exe";
+    private string unityAppPath = "not found";
 
     void Start()
     {
         Button launchButton = GetComponent<Button>();
+
+        //Parse the command line argument -filepath
+        string[] args = System.Environment.GetCommandLineArgs();
         launchButton.onClick.AddListener(OnClick);
+        
+        UnityEngine.Debug.Log("Command line argument length:" + args.Length);
+        if (args.Length > 1)
+        {
+            for (int i = 1; i < args.Length; i++)
+            {
+                UnityEngine.Debug.Log("Command line argument: " + args[i]);
+                if (args[i] == "-filePath" && i + 1 < args.Length)
+                {
+                    unityAppPath = args[i + 1];
+                    UnityEngine.Debug.Log("Unity app path: " + unityAppPath);
+                    break;
+                }
+            }
+        }
     }
 
     void OnClick()
     {
-        unityAppPath = "C:/Users/Gage Unruh/Tin Whisker POC/Builds/Final/Tin Whisker POC.exe";
+        if (unityAppPath == "not found")
+        {
+            UnityEngine.Debug.LogError("Unity app path not found");
+            return;
+        }
         for (int i = 1; i <= numSimulations; i++)
         {
             Process process = new Process();
             process.StartInfo.FileName = unityAppPath;
-            //process.StartInfo.Arguments = "-simNumber" + i + " -batchmode -nographics";
-            process.StartInfo.Arguments = "-simNumber " + i;
+            process.StartInfo.Arguments = " -batchmode -nographics -simNumber" + i;
+            //process.StartInfo.Arguments = "-simNumber " + i;
             process.StartInfo.UseShellExecute = false;
             //process.StartInfo.CreateNoWindow = true;
             process.Start();
