51675e07b873c9709258bad9db7bfaa32c05f340
TrevorBuchanan <123215971+TrevorBuchanan@users.noreply.github.com>
2024-09-30 09:48:06 -0700

    Adding batches

--- Tin Whisker POC/Assets/Scripts/MonteCarloSim.cs
+++ Tin Whisker POC/Assets/Scripts/MonteCarloSim.cs
@@ -14,27 +14,47 @@ public class MonteCarloSim : MonoBehaviour
 {
     public int numSimulations = 2; // 2 Default
     public bool IsSimulationEnded;
+
     private string[] layerNames;
     private WhiskerSim whiskerSim;
+    private int maxBatchSize = 10;
 
     public void RunMonteCarloSim(WhiskerSim whiskerSim, ref int simNumber, float duration) {
         IsSimulationEnded = false;
         this.whiskerSim = whiskerSim;
         MakeLayerNames();
         Time.timeScale = 10.0f;
-        for (int i = 0; i < numSimulations; i++) {
-            this.whiskerSim.RunSim(ref simNumber, duration, layerNames[i], false);
+        StartCoroutine(RunSimulationsInBatches(simNumber, duration));
+    }
+
+
+    IEnumerator RunSimulationsInBatches(int simNumber, float duration) {
+        int totalSimulations = numSimulations;
+        int batchStart = 0;
+
+        while (batchStart < totalSimulations) {
+            int batchEnd = Mathf.Min(batchStart + maxBatchSize, totalSimulations);
+            Debug.Log($"Running simulations from {batchStart} to {batchEnd - 1}");
+
+            for (int i = batchStart; i < batchEnd; i++) {
+                this.whiskerSim.RunSim(ref simNumber, duration, layerNames[i], false);
+            }
+
+            yield return new WaitUntil(() => whiskerSim.NumberSimsRunning == 0);
+
+            batchStart = batchEnd;
         }
+
         StartCoroutine(EndActions());
     }
 
     IEnumerator EndActions() {
-        yield return new WaitUntil(() => whiskerSim.NumberSimsRunning == 0);
         Debug.Log("End of monte carlo sim");
         Time.timeScale = 1.0f;
         IsSimulationEnded = true;
-    }
-
+        yield return null;
+    }    
+    
     private void MakeLayerNames() {
         layerNames = new string[numSimulations];
         int[] possibleLayerNums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
